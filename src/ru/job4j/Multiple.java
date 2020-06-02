3d0f7c8de1f8718ceaa30f8bd6b28e1e2a683dbf
AndreyShulgin <neonod404@gmail.com>
2020-06-02 12:33:01 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Multiple {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            System.out.println("x * 1 = " + x);
+        }
+    }
+}
