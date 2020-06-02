dca493b93c55fb43777d8b15d9c8da32faebcbe9
AndreyShulgin <neonod404@gmail.com>
2020-06-02 12:36:01 +0300

    3.4. Заголовок в файле java [#159036]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("02.06.2020");
+    }
+}
