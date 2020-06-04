0eb393cf634e6344f17012dfed3add545d84af27
AndreyShulgin <neonod404@gmail.com>
2020-06-04 19:06:07 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I am a newbie. ";
+        int year = 2020;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
