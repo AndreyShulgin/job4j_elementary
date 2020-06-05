c0ca81df59b7ea39b5273bbe34c30e7414ffdad8
AndreyShulgin <neonod404@gmail.com>
2020-06-05 22:57:17 +0300

    1.1. Входные данные в тесте [#297750]

---
+++ src/ru/job4j/calculate/X2.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class X2 {
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 1;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+    }
+}
