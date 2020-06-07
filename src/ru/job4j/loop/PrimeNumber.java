7db5035c87875c01b8e9bd201460d39533399205
AndreyShulgin <neonod404@gmail.com>
2020-06-07 18:21:37 +0300

    5.6. Простые числа. [#156314]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+/**
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class PrimeNumber {
+
+    public int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
