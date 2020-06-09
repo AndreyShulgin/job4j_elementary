6ecccac5052a4e95a27778486b767a89e3f36259
AndreyShulgin <neonod404@gmail.com>
2020-06-09 13:03:20 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Класс ArrayDefinition показывает как создаются массивы
+ *
+ * @author Andrey Shulgin (neonod404@gmail.com)
+ */
+public class ArrayDefinition {
+
+    /**
+     * Метод main
+     *
+     * @param args - args
+     */
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
