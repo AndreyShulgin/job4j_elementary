220cfb3cf2cb5fd8f02cc87c12b6b0902fc4c2fa
AndreyShulgin <neonod404@gmail.com>
2020-06-30 12:34:56 +0300

    6.7.2. Моно столбец в матрице. [#214127]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -8,16 +8,34 @@ package ru.job4j.array;
 public class MatrixCheck {
 
     /**
-     * Метод проверяет что значения всеъ элементов во всех ячейках заданной строки 'X'
+     * Метод проверяет что значения всех элементов во всех ячейках заданного столбца 'X'
      *
      * @param board - массив
-     * @param row   - строка
+     * @param row   - столбец
      * @return - true если все элементы 'X'
      */
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int index = 0; index < board[row].length; index++) {
             if (board[row][index] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод проверяет что значения всех элементов во всех столбцах строки column равно 'X'
+     *
+     * @param board  - массив
+     * @param column - строка
+     * @return - true если все элементы 'X'
+     */
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (char[] chars : board) {
+            if (chars[column] != 'X') {
                 result = false;
                 break;
             }
--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -30,4 +30,26 @@ public class MatrixCheckTest {
         assertThat(result, is(false));
     }
 
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'}
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasNotMonoVertical() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', ' ', ' '},
+                {' ', ' ', ' '},
+                {'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 0);
+        assertThat(result, is(false));
+    }
 }
